1f01ccd2862a01407fe64c1a9b9cb95d06eacc5e
VO THANH PHONG <vophong1612@gmail.com>
2020-10-25 20:42:12 +0700

    change database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors=require("cors")
 const app = express(cors({
-    origin:"http://localhost:3000"
+    origin:"http://*.herokuapp.com"
 }));
 const bodyParser = require('body-parser');
 
