01e54c5cca3d2e55ce5222e1e82bc8f3a18f4472
VO THANH PHONG <vophong1612@gmail.com>
2020-10-26 10:40:30 +0700

    fix packagejson

--- server.js
+++ server.js
@@ -17,5 +17,10 @@ app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 })
 
-app.listen(port);
-console.log('RESTful API server started on: ' + port);
+app.listen(port, () => {
+    console.log('RESTful API server started on: ' + port);
+});
+
+//chỉnh lại cách query db
+//chỉnh localhost -> 0.0.0.0
+ 
