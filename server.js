12e203e064030731dc1ca8472b4de151ccf05d73
VO THANH PHONG <vophong1612@gmail.com>
2020-11-04 12:50:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const cookieParser = require("cookie-parser");
 
 app.use(function (req, res, next) {
     // Mọi domain
-    res.header("Access-Control-Allow-Origin", "https://sprint-retrospective-web.herokuapp.com");
+//     res.header("Access-Control-Allow-Origin", "*");
 
     // Domain nhất định
-    // res.header("Access-Control-Allow-Origin", "https://freetuts.net");
+    res.header("Access-Control-Allow-Origin", "https://sprint-retrospective-web.herokuapp.com");
 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
