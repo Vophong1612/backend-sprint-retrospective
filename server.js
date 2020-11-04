448f453cb0e27464ca0b9f8098567c356d270dd0
VO THANH PHONG <vophong1612@gmail.com>
2020-11-04 12:59:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://sprint-retrospective-web.herokuapp.com");
 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
+    res.header("Access-Control-Allow-Credentials", true);
+    
     next();
 });
 
